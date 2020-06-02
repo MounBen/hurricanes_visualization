3f9a46319abbecfe73f6ab9c77c03ce28afd2355
MounBen <mounir.benheddi@gmail.com>
2020-06-02 12:57:35 +0200

    Revert changes with flask

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from bokeh.server.server import Server
 from tornado.ioloop import IOLoop
 from threading import Thread
 
+app = Flask(__name__)
+
 
 def spawnapp(doc):
     make_start_end_figure(doc)
@@ -15,14 +17,34 @@ def tracksapp(doc):
     make_tracks_figure(doc)
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html", template="Flask")
+
+
+@app.route('/spawns/', methods=['GET'])
+def spawn_page():
+    script = server_document('/spawns')
+    return render_template("embed.html", script=script, template="Flask")
+
+
+@app.route('/tracks/', methods=['GET'])
+def tracks_page():
+    script = server_document('/tracks')
+    return render_template("embed.html", script=script, template="Flask")
+
+
 def bk_worker():
     server = Server({'/spawns': spawnapp, '/tracks': tracksapp}, io_loop=IOLoop(),
                     host="hurricanes-visualization.herokuapp.com", port="$PORT",
-                    allow_websocket_origin=["https://hurricanes-visualization.herokuapp.com/"],
+                    allow_websocket_origin=["https://hurricanes-visualization.herokuapp.com/",
+                                            "localhost:8000"],
                     usexheaders=True)
     server.start()
     server.io_loop.start()
 
 
+Thread(target=bk_worker).start()
+
 if __name__ == '__main__':
-    Thread(target=bk_worker).start()
+    app.run(threaded=True, port=33507)
