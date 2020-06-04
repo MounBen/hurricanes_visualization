ff9d1b16ae58b0d8a2476ee94791ec3d8051efd4
MounBen <mounir.benheddi@gmail.com>
2020-06-04 13:51:49 +0200

    initial commit

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-web: gunicorn app:app --log-level=debug
-
-worker: bokeh serve --port=$PORT --num-procs=0 --allow-websocket-origin=hurricanes-visualization.herokuapp.com \
-        --address=0.0.0.0 --use-xheaders app.py
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 from flask import Flask, render_template
-from workflow.make_figures import make_start_end_figure, make_tracks_figure
 
 from bokeh.embed import server_document
-from bokeh.server.server import Server
-from tornado.ioloop import IOLoop
-from threading import Thread
 
 app = Flask(__name__)
-
-
-def spawnapp(doc):
-    make_start_end_figure(doc)
-
-
-def tracksapp(doc):
-    make_tracks_figure(doc)
 
 
 @app.route('/', methods=['GET'])
@@ -24,23 +12,15 @@ def home():
 
 @app.route('/spawns/', methods=['GET'])
 def spawn_page():
-    script = server_document('/spawns')
+    script = server_document('https://hurricanes-bokeh.herokuapp.com/spawns')
     return render_template("embed.html", script=script, template="Flask")
 
 
 @app.route('/tracks/', methods=['GET'])
 def tracks_page():
-    script = server_document('/tracks')
+    script = server_document('https://hurricanes-bokeh.herokuapp.com//tracks')
     return render_template("embed.html", script=script, template="Flask")
 
 
-def bk_worker():
-    server = Server({'/spawns': spawnapp, '/tracks': tracksapp}, io_loop=IOLoop())
-    server.start()
-    server.io_loop.start()
-
-
-Thread(target=bk_worker).start()
-
 if __name__ == '__main__':
-    app.run(threaded=True, port=33507)
+    app.run(threaded=True)
